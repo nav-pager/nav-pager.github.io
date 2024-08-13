0323043ab9282d8c4a27492bbaa8d7287471b2b0
Danny Engelman <danny@engelman.nl>
2024-08-13 19:06:21 +0200

    <nav-pager> Web Component

--- element.js
+++ element.js
@@ -1,21 +1,23 @@
 // define will automagically upgrade any existing (but undefined) Web Components in the DOM
 customElements.define('nav-pager', class extends HTMLElement {
     get selected() {
-        return this.querySelector("nav[selected]"); // no need for jQuery, available since IE9 (that was 2011)
+        return this.querySelector("nav[selected]"); // no need for jQuery, querySelector available since IE9 (that was 2011)
     }
-    get navs() {
+
+    get navElements() {
         return (this.navsquery = this.navsquery || [...this.querySelectorAll("nav")]) // convert NodeList to Array
         // do not cache when user/code can dynamically add <nav> elements, 
         // but this component does not handle that scenario, needs (partial) re-rendering
     }
-    select(nav = this.selected || this.navs[0]) {
+
+    select(nav = this.selected || this.navElements[0]) {
         // remove "selected" from <span> pagenumbers and <nav>
-        [...this.spans, ...this.navs].map(el => {
+        [...this.spanElements, ...this.navElements].map(el => {
             el.removeAttribute("selected");
             el.removeAttribute("aria-current"); // they are not on <span> but on <nav> We'll fix that later
         });
         // set selected on <span> page numbers and this <nav>
-        [...nav.spans, nav].map(el => {
+        [...nav.spanElements, nav].map(el => {
             el.setAttribute("selected", true)
         });
         nav.setAttribute("aria-current", "page");
@@ -29,28 +31,30 @@ customElements.define('nav-pager', class extends HTMLElement {
     // DO NOT read Attributes or do DOM operations in the (default) constructor!!! (except for your own added shadowDOM)
     // your code will break on document.createElement("your-component") ... which you most likely did not test
     connectedCallback() {
-        //console.log(this.id, this.innerHTML.length); // most developers do not understand why this is 0 sometimes, see link below
-        setTimeout(() => this.render()); // wait till <nav-pager> innerHTML is parsed. See link below
+        //console.log(this.id, this.innerHTML.length); // most developers do not understand why this is 0 sometimes. See link
+        setTimeout(() => this.render()); // wait till <nav-pager> innerHTML is parsed. See link
     }
 
     render() {
         this.render = () => { } // render once! because connectedCallback will be called on DOM mutations (like drag/drop)
-        this.spans = []; // keep record of all <span> pagenumbers/titles
-        this.pagelabel = this.getAttribute("pagelabel") || "Page: ";
 
-        // THE Helper Function you will use in every Web Component (well, okay, maybe not every)
+        this.pagelabel = this.getAttribute("pagelabel") || "Page: "; // default prefix for pagenumbers
+        this.spanElements = []; // keep record of all <span> pagenumbers/titles
+
+        // THE Helper Function you want to use in every Web Component (well, okay, maybe not every)
         const createElement = (tag, props = {}) => Object.assign(document.createElement(tag), props);
 
-        const createPageSelector = () => this.navs.map((nav, idx) => {
+        const createPageSelector = () => this.navElements.map((nav, idx) => {
             let span = createElement("span", {
                 textContent: nav.title || ++idx, // use .title or new page number
-                part: "pagenr", // allow styling from Global CSS, what the other half of the blogs whine about
-                onclick: evt => this.select(nav) // no need for addEventListener, there is only one onclick required PER span
-                // note: No removeListener required (in a disconnectedCallback), 
-                // this handler is garbage collected when the <span> is removed
+                part: "pagenr", // allow styling from Global CSS, what many blogs whine about
+                onclick: evt => this.select(nav) // no need for addEventListener, there is only one onclick PER span
+                // No removeListener required (in a disconnectedCallback), handler is garbage collected when the <span> is removed
             }); // createElement <span>
-            this.spans.push(span); // store all <span> on <nav-pager>
-            (nav.spans = nav.spans || []).push(span); // store all <span> on matching <nav>
+
+            this.spanElements.push(span); // store all <span> on <nav-pager>
+            (nav.spanElements = nav.spanElements || []).push(span); // store all <span> on matching <nav>
+
             return span; // return all <span> as Array
         }); // createPageSelector() returns Array of <span>
 
@@ -75,7 +79,8 @@ customElements.define('nav-pager', class extends HTMLElement {
                 this.pagelabel, ...createPageSelector(), // spread all <span>
             ); // append
 
-        this.select();
+        this.select(); // initial display of a page
 
-    } // render
+    } // render once
+
 }); // define <nav-pages>
